3a8635f3b62b2d3fa9aad22bb3e6824dea4aab13
Helen Hood <helen.c.hood@gmail.com>
2014-07-07 13:49:56 -0400

    Validate presence of book on Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
   validates :rating, presence: true, inclusion: { within: 1..5 }
   validates :body, presence: true
+  validates :book, presence: true
 
   belongs_to :book
 end
